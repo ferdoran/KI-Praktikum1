529086ea260d24656d51852aecf98bfea848eac0
Mahdi <Mahdi@MahdiSurface.schneider-it.xyz>
2015-10-30 15:03:27 +0100

    A* weiter optimiert AStarTest lauffähig getestet

---
+++ Pfadsuche/src/algorithms/NavEdge.java
@@ -0,0 +1,28 @@
+/*
+ * TH Koeln Informatik BA
+ * 5. Semester KI Praktikum
+ * Praktikum 1, Aufgabe 1
+ * Roland Mueller & Stephan Schneider
+ */
+package algorithms;
+
+/**
+ *
+ * @author Mahdi
+ */
+class NavEdge {
+    protected String from;
+    protected String to;
+    protected double cost;
+
+    NavEdge(String firstId, String secondId, double cost) {
+        this.from = firstId;
+        this.to = secondId;
+        this.cost = cost;
+    }
+
+    NavEdge(String firstId, String secondId) {
+        this.from = firstId;
+        this.to = secondId;
+    }
+}
