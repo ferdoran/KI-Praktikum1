b075ddbcdf709e8f14706339c7b1814197b36dcd
Mahdi Avendesora <github@mahdi-avendesora.eu>
2015-11-08 21:28:11 +0100

    WorldTest bearbeitet

--- Pfadsuche/src/test/WorldTest.java
+++ Pfadsuche/src/test/WorldTest.java
@@ -1,6 +1,6 @@
 package test;
 import algorithms.agent.World;
-import data.*;
+import java.awt.geom.Point2D;
 
 /**
  *
@@ -9,7 +9,8 @@ import data.*;
 public class WorldTest {
     public static void main(String[] args){
         World world = new World();
-        world.setAgentPosition(new Point(100,534, "S"));
+        Point2D.Double start = new Point2D.Double(100,534);
+        world.setAgentPosition(start);
         
         System.out.print(world.getAvailablePoints().toString());
         
